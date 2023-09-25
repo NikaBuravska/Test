49db41a7711a33423598f0f5d48050681d1b1e95
NikaBuravska <buravska.nika@gmail.com>
2023-09-25 13:30:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Слава Україні!");
+
+        int celsiumValue = -5;
+        float fahrenValue = ((celsiumValue * 9)/5)+32F;
+        float kelvinValue = (float) (celsiumValue + 273.16);
+        int sideA = 10;
+        int sideB = 2;
+        int areaTr = (sideA * sideB)/2;
+
+
+
+        System.out.println(celsiumValue+" "+"Celsius");
+        System.out.println(fahrenValue+" "+"Fahrenheit");
+        System.out.println(kelvinValue+" "+"Kelvin");
+        System.out.println(areaTr + " "+"Triange's area");
+
+
+
+    }
+}
